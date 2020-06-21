c2f818740f7acb9675211697c1f469ff12a8ea03
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-06-21 22:04:44 +0300

    Adds variables and const

--- js/main.js
+++ js/main.js
@@ -196,25 +196,8 @@ uploadFile.addEventListener('change', inputUploadClickHandler);
 
 var MAX_VALUE = 100;
 var MIN_VALUE = 25;
+var SCALE_STEP = 25;
 var controlSmaller = imageEditingForm.querySelector('.scale__control--smaller');
 var controlBigger = imageEditingForm.querySelector('.scale__control--bigger');
 var controlValue = imageEditingForm.querySelector('.scale__control--value');
-
-controlSmaller.addEventListener('click', function () {
-  var currentValue = parseInt(controlValue.value, 10);
-
-  if (currentValue > MIN_VALUE) {
-    currentValue -= 25;
-    controlValue.value = currentValue + '%';
-  }
-
-});
-
-controlBigger.addEventListener('click', function () {
-  var currentValue = parseInt(controlValue.value, 10);
-
-  if (currentValue < MAX_VALUE) {
-    currentValue += 25;
-    controlValue.value = currentValue + '%';
-  }
-});
+var imageUpload = imageEditingForm.querySelector('img');
