944a2bd1440ffb5c0c5e987504aa268cb40f23e8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-06-29 22:54:05 +0300

    Finds DOM elements

--- js/preview.js
+++ js/preview.js
@@ -1,5 +1,15 @@
 'use strict';
 
 (function () {
+  var body = document.querySelector('body');
+  var previewPicture = document.querySelector('.big-picture');
+  var bigPicture = previewPicture.querySelector('.big-picture__img img');
+  var likesCount = previewPicture.querySelector('.likes-count');
+  var commentsCount = previewPicture.querySelector('.comments-count');
+  var socialComments = previewPicture.querySelector('.social__comments');
+  var socialCaption = previewPicture.querySelector('.social__caption');
+  var socialCommentCount = previewPicture.querySelector('.social__comment-count');
+  var commentsLoader = previewPicture.querySelector('.comments-loader');
 
+  previewPicture.classList.remove('hidden');
 })();
