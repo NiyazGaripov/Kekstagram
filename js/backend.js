261fcab0738aae4f3e2e61c5b02d871cf99f53a3
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-05 16:52:04 +0300

    Adds status codes

--- js/backend.js
+++ js/backend.js
@@ -4,4 +4,12 @@
     POST: 'https://javascript.pages.academy/kekstagram',
     GET: 'https://javascript.pages.academy/kekstagram/data',
   };
+
+  var StatusCode = {
+    OK: 200,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER: 500,
+  };
+
 })();
