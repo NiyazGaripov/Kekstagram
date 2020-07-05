109395455ebd02bb66bd8303f7a33082c059f8c5
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-05 16:51:41 +0300

    Adds methods

--- js/backend.js
+++ js/backend.js
@@ -1,4 +1,7 @@
 'use strict';
 (function () {
-
+  var UrlTypes = {
+    POST: 'https://javascript.pages.academy/kekstagram',
+    GET: 'https://javascript.pages.academy/kekstagram/data',
+  };
 })();
