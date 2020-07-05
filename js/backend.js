cb5276d3960b5fff95d25da7fdeb8060ffbd1672
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-05 22:21:24 +0300

    Receives data from the server

--- js/backend.js
+++ js/backend.js
@@ -13,4 +13,44 @@
   };
 
   var TIMEOUT = 10000;
+
+  var loadData = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT;
+
+    xhr.open('GET', UrlTypes.GET);
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case StatusCode.OK:
+          onLoad(xhr.response);
+          break;
+        case StatusCode.BAD_REQUEST:
+          onError('Сервер не смог обработать ваш запрос. Попробуйте открыть сайт другим браузером.');
+          break;
+        case StatusCode.NOT_FOUND:
+          onError('Запрашиваемая страница не найдена. Проверьте корректность введённого адреса страницы.');
+          break;
+        case StatusCode.INTERNAL_SERVER:
+          onError('Сервер не отвечает. Повторите попытку позже.');
+          break;
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения! Проверьте соединение с интернетом');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.send();
+  };
+
+  window.backend = {
+    load: loadData,
+  };
+
 })();
