82d7b4c015964a2299c1e1299f70c297a9d6d35b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-11 12:17:57 +0300

    Creates function setDefaultValue

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,24 @@
 'use strict';
 
 (function () {
+  var DEFAULT_EFFECT_VALUE = 100;
 
+  var imageEditingForm = document.querySelector('.img-upload__overlay');
+  var imageUploadPreview = imageEditingForm.querySelector('.img-upload__preview img');
+  var effectLevelValue = imageEditingForm.querySelector('.effect-level__value');
+  var effectLevelPin = imageEditingForm.querySelector('.effect-level__pin');
+  var effectLevelDepth = imageEditingForm.querySelector('.effect-level__depth');
+
+
+  var setDefaultValue = function () {
+    effectLevelValue.value = DEFAULT_EFFECT_VALUE;
+    effectLevelPin.style.left = DEFAULT_EFFECT_VALUE + '%';
+    effectLevelDepth.style.width = DEFAULT_EFFECT_VALUE + '%';
+    imageUploadPreview.style.filter = '';
+  };
+
+
+  window.utils = {
+    setDefaultValue: setDefaultValue,
+  };
 })();
