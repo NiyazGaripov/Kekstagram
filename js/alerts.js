f79f57d8fd5da5647f0c498d360042dae5ad0f99
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-11 21:14:44 +0300

    Creates function renderSuccessAlert

--- js/alerts.js
+++ js/alerts.js
@@ -1,5 +1,15 @@
 'use strict';
 
 (function () {
+  var main = document.querySelector('main');
+  var successAlertTemplate = document.querySelector('#success');
+  var successAlert = successAlertTemplate.content.querySelector('.success');
 
+  var renderSuccessAlert = function () {
+    main.appendChild(successAlert);
+  };
+
+  window.alerts = {
+    renderSuccess: renderSuccessAlert,
+  };
 })();
