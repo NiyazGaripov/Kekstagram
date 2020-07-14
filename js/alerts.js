1377a3e4374e98f6829b7d7d2dadbd93087fe769
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-14 21:45:22 +0300

    Refactors module alerts

--- js/alerts.js
+++ js/alerts.js
@@ -2,21 +2,12 @@
 
 (function () {
   var main = document.querySelector('main');
-  var successAlertTemplate = document.querySelector('#success');
-  var successAlert = successAlertTemplate.content.querySelector('.success');
-  var errorAlertTemplate = document.querySelector('#error');
-  var errorAlert = errorAlertTemplate.content.querySelector('.error');
 
-  var renderSuccessAlert = function () {
-    main.appendChild(successAlert);
-  };
-
-  var renderErrorAlert = function () {
-    main.appendChild(errorAlert);
+  var renderAlert = function (alert) {
+    main.appendChild(alert);
   };
 
   window.alerts = {
-    renderSuccess: renderSuccessAlert,
-    renderError: renderErrorAlert,
+    render: renderAlert,
   };
 })();
