e9872c7d340fd371c5b58c3078456cb24c8cc965
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-14 21:12:34 +0300

    Creates function renderErrorAlert

--- js/alerts.js
+++ js/alerts.js
@@ -4,12 +4,19 @@
   var main = document.querySelector('main');
   var successAlertTemplate = document.querySelector('#success');
   var successAlert = successAlertTemplate.content.querySelector('.success');
+  var errorAlertTemplate = document.querySelector('#error');
+  var errorAlert = errorAlertTemplate.content.querySelector('.error');
 
   var renderSuccessAlert = function () {
     main.appendChild(successAlert);
   };
 
+  var renderErrorAlert = function () {
+    main.appendChild(errorAlert);
+  };
+
   window.alerts = {
     renderSuccess: renderSuccessAlert,
+    renderError: renderErrorAlert,
   };
 })();
