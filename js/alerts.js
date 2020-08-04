992600a2e41dfdc2a5e026191bcc9fc3c619048d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-04 20:54:14 +0300

    Creates function createAlertElement

--- js/alerts.js
+++ js/alerts.js
@@ -11,8 +11,23 @@
     alert.remove();
   };
 
+  var createAlertElement = function (message) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 1; margin: 0 auto; text-align: center;';
+    node.style.position = 'absolute';
+    node.style.right = 0;
+    node.style.left = 0;
+    node.style.fontSize = '30px';
+    node.style.backgroundColor = message ? 'red' : 'green';
+
+    node.textContent = message ? message : 'Данные успешно отправлены';
+
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
   window.alerts = {
     render: renderAlert,
     remove: removeAlert,
+    create: createAlertElement,
   };
 })();
