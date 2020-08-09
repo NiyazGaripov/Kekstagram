d673cdc1d7d07de89e7e28949ce7bfdf1fa6b381
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-09 14:35:56 +0300

    Finds DOM elements

--- js/image.js
+++ js/image.js
@@ -3,4 +3,8 @@
 (function () {
   var FILE_TYPES = ['svg', 'jpg', 'jpeg', 'png'];
 
+  var fileChooser = document.querySelector('#upload-file');
+  var preview = document.querySelector('.img-upload__preview img');
+  var effectsPreview = document.querySelectorAll('.effects__preview');
+
 })();
