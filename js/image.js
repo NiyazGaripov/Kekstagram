4c28d7cf0f6062d131293913064b334a313f3b25
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-09 14:36:49 +0300

    Adds listener change

--- js/image.js
+++ js/image.js
@@ -7,4 +7,25 @@
   var preview = document.querySelector('.img-upload__preview img');
   var effectsPreview = document.querySelectorAll('.effects__preview');
 
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+        effectsPreview.forEach(function(item) {
+          item.style.backgroundImage = `url(${reader.result})`;
+        })
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
 })();
